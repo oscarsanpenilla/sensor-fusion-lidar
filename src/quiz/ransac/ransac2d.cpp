23cd507ccc8f1951faa285a0ff3fe8207b3fa62e
Oscar Sandoval  Penilla <oscarsanpenilla@gmail.com>
2021-05-03 00:22:45 -0700

    Implement RANSAC from scratch

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -10,38 +10,36 @@
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData()
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
-  	// Add inliers
-  	float scatter = 0.6;
-  	for(int i = -5; i < 5; i++)
-  	{
-  		double rx = 2*(((double) rand() / (RAND_MAX))-0.5);
-  		double ry = 2*(((double) rand() / (RAND_MAX))-0.5);
-  		pcl::PointXYZ point;
-  		point.x = i+scatter*rx;
-  		point.y = i+scatter*ry;
-  		point.z = 0;
+	// Add inliers
+	float scatter = 0.6;
+	for (int i = -5; i < 5; i++)
+	{
+		double rx = 2 * (((double)rand() / (RAND_MAX)) - 0.5);
+		double ry = 2 * (((double)rand() / (RAND_MAX)) - 0.5);
+		pcl::PointXYZ point;
+		point.x = i + scatter * rx;
+		point.y = i + scatter * ry;
+		point.z = 0;
 
-  		cloud->points.push_back(point);
-  	}
-  	// Add outliers
-  	int numOutliers = 10;
-  	while(numOutliers--)
-  	{
-  		double rx = 2*(((double) rand() / (RAND_MAX))-0.5);
-  		double ry = 2*(((double) rand() / (RAND_MAX))-0.5);
-  		pcl::PointXYZ point;
-  		point.x = 5*rx;
-  		point.y = 5*ry;
-  		point.z = 0;
+		cloud->points.push_back(point);
+	}
+	// Add outliers
+	int numOutliers = 10;
+	while (numOutliers--)
+	{
+		double rx = 2 * (((double)rand() / (RAND_MAX)) - 0.5);
+		double ry = 2 * (((double)rand() / (RAND_MAX)) - 0.5);
+		pcl::PointXYZ point;
+		point.x = 5 * rx;
+		point.y = 5 * ry;
+		point.z = 0;
 
-  		cloud->points.push_back(point);
+		cloud->points.push_back(point);
+	}
+	cloud->width = cloud->points.size();
+	cloud->height = 1;
 
-  	}
-  	cloud->width = cloud->points.size();
-  	cloud->height = 1;
-
-  	return cloud;
-
+	return cloud;
 }
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D()
@@ -50,38 +48,63 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D()
 	return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
 }
 
-
 pcl::visualization::PCLVisualizer::Ptr initScene()
 {
-	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("2D Viewer"));
-	viewer->setBackgroundColor (0, 0, 0);
-  	viewer->initCameraParameters();
-  	viewer->setCameraPosition(0, 0, 15, 0, 1, 0);
-  	viewer->addCoordinateSystem (1.0);
-  	return viewer;
+	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("2D Viewer"));
+	viewer->setBackgroundColor(0, 0, 0);
+	viewer->initCameraParameters();
+	viewer->setCameraPosition(0, 0, 15, 0, 1, 0);
+	viewer->addCoordinateSystem(1.0);
+	return viewer;
 }
 
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
+	auto startTime = std::chrono::steady_clock::now();
+
 	std::unordered_set<int> inliersResult;
+	auto generateRandNum = [=](int max, int min) { return rand() % (max - min + 1) + min; };
 	srand(time(NULL));
-	
+
 	// TODO: Fill in this function
+	// For max iterations
+	for (size_t i = 0; i < maxIterations; i++)
+	{
+		// Randomly sample subset and fit line
+		pcl::PointXYZ point1, point2, point3;
+		int randNum1, randNum2;
+		do
+		{
+			randNum1 = generateRandNum(0, cloud->points.size() - 1);
+			randNum2 = generateRandNum(0, cloud->points.size() - 1);
+			point1 = cloud->points[randNum1];
+			point2 = cloud->points[randNum2];
+		} while (randNum1 == randNum2);
 
-	// For max iterations 
-
-	// Randomly sample subset and fit line
-
-	// Measure distance between every point and fitted line
-	// If distance is smaller than threshold count it as inlier
-
+		double A = point1.y - point2.y;
+		double B = point2.x - point1.x;
+		double C = point1.x * point2.y - point2.x * point1.y;
+		// Measure distance between every point and fitted line
+		std::unordered_set<int> inliers;
+		for (size_t i = 0; i < cloud->points.size(); ++i)
+		{
+			point3 = cloud->points[i];
+			double dist = fabs(A * point3.x + B * point3.y + C) / sqrt(A * A + B * B);
+			// If distance is smaller than threshold count it as inlier
+			if (dist <= distanceTol)
+				inliers.insert(i);
+		}
+		if (inliers.size() > inliersResult.size())
+			inliersResult = inliers;
+	}
+	auto endTime = std::chrono::steady_clock::now();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+	std::cout << "RANSAC took: " << elapsedTime.count() << " miliseconds\n";
 	// Return indicies of inliers from fitted line with most inliers
-	
 	return inliersResult;
-
 }
 
-int main ()
+int main()
 {
 
 	// Create viewer
@@ -89,38 +112,35 @@ int main ()
 
 	// Create data
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
-	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 0, 0);
+	std::unordered_set<int> inliers = Ransac(cloud, 10, 0.5);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
 
-	for(int index = 0; index < cloud->points.size(); index++)
+	for (int index = 0; index < cloud->points.size(); index++)
 	{
 		pcl::PointXYZ point = cloud->points[index];
-		if(inliers.count(index))
+		if (inliers.count(index))
 			cloudInliers->points.push_back(point);
 		else
 			cloudOutliers->points.push_back(point);
 	}
 
-
 	// Render 2D point cloud with inliers and outliers
-	if(inliers.size())
+	if (inliers.size())
 	{
-		renderPointCloud(viewer,cloudInliers,"inliers",Color(0,1,0));
-  		renderPointCloud(viewer,cloudOutliers,"outliers",Color(1,0,0));
+		renderPointCloud(viewer, cloudInliers, "inliers", Color(0, 1, 0));
+		renderPointCloud(viewer, cloudOutliers, "outliers", Color(1, 0, 0));
 	}
-  	else
-  	{
-  		renderPointCloud(viewer,cloud,"data");
-  	}
-	
-  	while (!viewer->wasStopped ())
-  	{
-  	  viewer->spinOnce ();
-  	}
-  	
+	else
+	{
+		renderPointCloud(viewer, cloud, "data");
+	}
+
+	while (!viewer->wasStopped())
+	{
+		viewer->spinOnce();
+	}
 }
